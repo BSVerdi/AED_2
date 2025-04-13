78bd678ecfa22a5a0306bd82dd1c9bc3282bfa11
Breno Verdi <brenoverdi2005@gmail.com>
2025-04-13 18:07:57 -0300

    some changes

--- Atividade_1/main.c
+++ Atividade_1/main.c
@@ -6,7 +6,7 @@ const long int SIZE = 100000000;
 
 
 int separa(long int *array, int left, int right) {
-    long int pivo = left + rand() % (right - left + 1);
+    long int pivo = array[right];
     int i = (left - 1);
     long int aux;
     
@@ -36,29 +36,95 @@ void quicksort(long int *array, int left, int right) {
 }
 
 
-int main() {
-    long int *array = (long int *)malloc(sizeof(long int) * SIZE);
-    clock_t tempo;
-    int i;
+int buscaLinear(long int *array, long int data) {
+    for (int i = 0; i < SIZE; i++) {
+        if (array[i] == data)
+            return i;
+    }
+    return -1;
+}
 
-    srand((unsigned)time(NULL));
+
+int buscaBinaria(long int *array, long int data) {
+    int left = 0;
+    int right = SIZE -1;
+    int mid;
     
-    printf("%ld", SIZE / 2);
-
-
-    for (i = 0; i < (SIZE / 2); i++) {
-        array[i] = rand() % SIZE;
-        array[i + (SIZE / 2)] = rand() % SIZE;
+    while (left <= right) {
+        mid = (left + right) / 2;
+        if (array[mid] == data)
+            return mid;
+        else if (array[mid] < data)
+            left = mid + 1;
+        else 
+            right = mid - 1;
     }
 
-    printf("Valores Criados!\n");
-    
-    quicksort(array, 0, SIZE);
-    
-    printf("Valores Ordenados!\n");
+    return -1;
+}
+
+
+int buscaBinariaRecursiva(long int *array, long int data, int left, int right) {
+    int mid = (left + right) / 2;
+
+    if (array[mid] == data)
+        return mid;
+    else if (array[mid] < data)
+        buscaBinariaRecursiva(array, data, mid + 1, right);
+    else
+        buscaBinariaRecursiva(array, data, left, mid - 1);
+
+    return -1;
+}
+
+
+int main() {
+    long int *array = (long int *)malloc(sizeof(long int) * SIZE);
+    long int num;
+    clock_t tempo;
+    int i, index;
+
+    srand((unsigned)time(NULL));
 
     for (i = 0; i < SIZE; i++)
-        printf(" %ld =>", array[i]);
+        array[i] = rand() % SIZE;
+
+    printf("Valores Criados!\n");
+    printf("Iniciando ordenacao\n");
+
+    tempo = clock();
+    quicksort(array, 0, SIZE - 1);
+    tempo = clock() - tempo;
+    
+    printf("Valores Ordenados!\nTempo de execucao: %.0lf ms\n", ((double)tempo)/(CLOCKS_PER_SEC/1000));
+
+    for (i = 0; i < 20; i++) {
+        num = rand() % SIZE;
+
+        printf("Ciclo %d\n", i + 1);
+        printf("num = %ld\n", num);
+
+        tempo = clock();
+        index = buscaLinear(array, num);
+        tempo = clock() - tempo;
+
+        printf("indice = %d\n", index);
+        printf("Tempo de execucao busca linear: %.0lf ms\n",  ((double)tempo)/(CLOCKS_PER_SEC/1000));
+        
+        tempo = clock();
+        buscaBinaria(array, num);
+        tempo = clock() - tempo;
+
+        printf("indice = %d\n", index);
+        printf("Tempo de execucao busca binaria: %.0lf ms\n",  ((double)tempo)/(CLOCKS_PER_SEC/1000));
+        
+        tempo = clock();
+        buscaBinariaRecursiva(array, num, 0, SIZE - 1);
+        tempo = clock() - tempo;
+
+        printf("indice = %d\n", index);
+        printf("Tempo de execucao busca binaria recursiva: %.0lf ms\n",  ((double)tempo)/(CLOCKS_PER_SEC/1000));
+    }
 
     return 0;
 }
